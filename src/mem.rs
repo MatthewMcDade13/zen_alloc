cfb4a08797a2c08b10ceb10de2ef43f07e77ebc6
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2024-06-17 06:48:28 -0700

    finishing up blockvec

--- src/mem.rs
+++ src/mem.rs
@@ -1,15 +1,16 @@
 use core::slice;
 use std::{
-    alloc::{alloc, alloc_zeroed, dealloc, Layout},
+    alloc::{alloc_zeroed, dealloc, Layout},
     marker::PhantomData,
-    ops::Index,
+    ops::Deref,
+    ptr::NonNull,
 };
 
 #[derive(Debug)]
 pub struct BlockVec {
     block_size: usize,
     len: usize,
-    buf: *mut u8,
+    buf: NonNull<u8>,
 
     _phantom: PhantomData<[u8]>,
 }
@@ -19,13 +20,44 @@ impl BlockVec {
         Self {
             block_size,
             len: 0,
-            buf: std::ptr::null_mut(),
+            buf: NonNull::dangling(),
             _phantom: PhantomData,
         }
     }
 
+    pub fn index<T>(&self, index: usize) -> &T {
+        self.view(index).to_ref()
+    }
+
+    fn view(&self, index: usize) -> BlockView {
+        let byte_offset = index * self.block_size();
+
+        assert!(
+            byte_offset < self.nbytes(),
+            "Index out of range: BlockVec[{}]. len: {}",
+            index,
+            self.len
+        );
+
+        unsafe {
+            let ptr = self.buf.as_ptr().add(byte_offset);
+            // let ptr = ptr.add(std::mem::size_of::<BlockHeader>());
+            // let offset = ptr.align_offset(std::mem::align_of::<BlockView>());
+            // let ptr = ptr.add(offset).cast::<BlockView>();
+
+            let ptr = ptr.cast::<BlockHeader>();
+            let header = std::ptr::read(ptr);
+            let ptr = ptr.add(1).cast::<u8>();
+            BlockView { ptr, header }
+        }
+    }
+
+    pub const fn bytes(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self.buf.as_ptr(), self.nbytes()) }
+    }
+
     pub const fn block_size(&self) -> usize {
-        self.block_size + std::mem::size_of::<BlockMeta>()
+        self.block_size + std::mem::size_of::<BlockHeader>()
     }
 
     pub const fn len(&self) -> usize {
@@ -38,16 +70,7 @@ impl BlockVec {
 
     #[inline]
     fn layout(&self) -> Layout {
-        Self::new_layout(self.nbytes())
-    }
-
-    #[inline]
-    fn new_layout(nbytes: usize) -> Layout {
-        Layout::array::<u8>(nbytes).expect("Layout::array => nbytes too big")
-    }
-
-    pub fn with_capacity(block_size: usize, capacity: usize) -> Self {
-        todo!()
+        Layout::array::<u8>(self.nbytes()).expect("Layout::array => nbytes too big")
     }
 
     /// Resizes inner buffer into a new allocated one with new size and
@@ -67,29 +90,6 @@ impl BlockVec {
     fn shrink(&mut self, nblocks: usize) {
         self.len -= nblocks;
     }
-    //     unsafe {
-    //         let layout_old = self.layout();
-    //         let nbytes_old = self.nbytes();
-    //
-    //         self.len -= nblocks;
-    //
-    //         let layout_new = self.layout();
-    //         let nbytes_new = self.nbytes();
-    //         let dst = alloc_zeroed(layout_new);
-    //
-    //         if dst.is_null() {
-    //             panic!("Out of memory!!");
-    //         }
-    //
-    //         let new_buf = copy_raw(dst, nbytes_new, self.buf, nbytes_old);
-    //
-    //         dealloc(self.buf, layout_old);
-    //         self.buf = new_buf;
-    //         // let dst = slice::from_raw_parts_mut(dst, nbytes_new);
-    //         // let src = slice::from_raw_parts(self.buf, nbytes_old);
-    //         // super::mem::copy(dst, src);
-    //     }
-    // }
 
     fn grow(&mut self, nblocks: usize) {
         unsafe {
@@ -106,10 +106,10 @@ impl BlockVec {
                 panic!("Out of memeory!!");
             }
 
-            let new_buf = copy_raw(dst, nbytes_new, self.buf, nbytes_old);
+            let new_buf = copy_raw(dst, nbytes_new, self.buf.as_ptr(), nbytes_old);
 
-            dealloc(self.buf, layout_old);
-            self.buf = new_buf;
+            dealloc(self.buf.as_ptr(), layout_old);
+            self.buf = NonNull::new_unchecked(new_buf);
             // let dst = slice::from_raw_parts_mut(dst, nbytes_new);
             // let src = slice::from_raw_parts(self.buf, nbytes_old);
             // super::mem::copy(dst, src);
@@ -118,8 +118,8 @@ impl BlockVec {
 
     fn deref_handle<T>(&self, handle: &BlockHandle<T>) -> &T {
         unsafe {
-            let ptr = self.buf.add(handle.id * self.block_size());
-            let ptr = ptr.add(std::mem::size_of::<BlockMeta>());
+            let ptr = self.buf.as_ptr().add(handle.header.id * self.block_size());
+            let ptr = ptr.add(std::mem::size_of::<BlockHeader>());
             let offset = ptr.align_offset(std::mem::align_of::<T>());
             let ptr = ptr.add(offset).cast::<T>();
             ptr.as_ref().expect("Attempt to deref null pointer")
@@ -129,24 +129,9 @@ impl BlockVec {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct BlockHandle<'alloc, T> {
-    id: usize,
-    parent: &'alloc BlockVec,
-    _phantom: PhantomData<&'alloc T>,
-}
-
 impl<'alloc, T> BlockHandle<'alloc, T> {
     pub fn get(&self) -> &T {
         self.parent.deref_handle(self)
-    }
-}
-
-impl Index<usize> for BlockVec {
-    type Output = BlockView;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        todo!()
     }
 }
 
@@ -157,37 +142,71 @@ impl Index<usize> for BlockVec {
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct BlockMeta {
+struct BlockHeader {
     id: usize,
     next: usize,
     inner_size: usize,
 }
 
-impl BlockMeta {
+impl BlockHeader {
     pub const fn alloc_size(&self) -> usize {
         std::mem::size_of::<Self>() + self.inner_size
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct BlockHandle<'alloc, T> {
+    header: BlockHeader,
+    parent: &'alloc BlockVec,
+    _phantom: PhantomData<T>,
+}
+
+impl<'alloc, T> Deref for BlockHandle<'alloc, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.parent.deref_handle(self)
+    }
+}
+
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct BlockView {
-    meta: BlockMeta,
+struct BlockView {
+    header: BlockHeader,
     ptr: *mut u8,
 }
 
 impl BlockView {
-    pub fn slice(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts(self.ptr, self.meta.inner_size) }
+    pub const fn alloc_size(&self) -> usize {
+        std::mem::size_of::<BlockHeader>() + self.header.inner_size
+    }
+    pub const fn slice(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self.ptr, self.header.inner_size) }
     }
 
     pub const fn inner_size(&self) -> usize {
-        self.meta.inner_size
+        self.header.inner_size
+    }
+
+    pub fn to_ref<'a, T>(self) -> &'a T {
+        unsafe { self.ptr.cast::<T>().as_ref().expect("Null pointer deref.") }
+    }
+
+    pub fn to_mut<'a, T>(self) -> &'a mut T {
+        unsafe { self.ptr.cast::<T>().as_mut().expect("Null pointer deref.") }
+    }
+
+    pub fn cast<T>(&self) -> &T {
+        unsafe { self.ptr.cast::<T>().as_ref().expect("Null pointer deref.") }
+    }
+
+    pub fn cast_mut<T>(&self) -> &mut T {
+        unsafe { self.ptr.cast::<T>().as_mut().expect("Null pointer deref.") }
     }
 }
 
-pub fn block_alloc_size(meta: &BlockMeta) -> usize {
-    std::mem::size_of::<BlockMeta>() + meta.inner_size
+pub fn block_alloc_size(meta: &BlockHeader) -> usize {
+    std::mem::size_of::<BlockHeader>() + meta.inner_size
 }
 
 pub fn copy_raw(dst_ptr: *mut u8, dst_len: usize, src_ptr: *const u8, src_len: usize) -> *mut u8 {
@@ -208,6 +227,6 @@ pub fn copy(dst: &mut [u8], src: &[u8]) {
 
 impl Drop for BlockVec {
     fn drop(&mut self) {
-        unsafe { dealloc(self.buf, self.layout()) }
+        unsafe { dealloc(self.buf.as_ptr(), self.layout()) }
     }
 }
